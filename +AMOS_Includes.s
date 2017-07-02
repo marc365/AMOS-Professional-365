b2f0dfe3f00588f96297ce1fe8999978744175e5
marc365 <marc.williams@hushmail.com>
2017-07-02 14:44:49 +0100

    Library Vector Offsets added

--- +AMOS_Includes.s
+++ +AMOS_Includes.s
@@ -20,8 +20,21 @@ VDemo:		equ 	0
 ROnly:		equ 	0
 		ENDC
 ;
+		Incdir  "includes/"
+		Include "lvo/exec_lib.i"
+		Include "lvo/dos_lib.i"
+		Include "lvo/layers_lib.i"
+		Include "lvo/graphics_lib.i"
+		Include "lvo/mathtrans_lib.i"
+		Include "lvo/rexxsyslib_lib.i"
+		Include "lvo/mathffp_lib.i"
+		Include "lvo/mathieeedoubbas_lib.i"
+		Include "lvo/intuition_lib.i"
+		Include "lvo/diskfont_lib.i"
+		Include "lvo/icon_lib.i"
+		Include "lvo/console_lib.i"
+
 		Include	"+Debug.s"		Just one flag
-		Include "+LibOffsets.s"		Library offsets
 		Include "+Equ.s"
 		RsSet	DataLong
 		Include	"+CEqu.s"
